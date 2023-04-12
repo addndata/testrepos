2111c5481f56938b558fda2d177f4d9cd95c46cd
addndata <111285262+addndata@users.noreply.github.com>
2023-04-13 00:16:19 +0300

    Create wa.sh

---
+++ wa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Oracle veritabanı bağlantı bilgileri
+db_username="paybito_db"
+db_password="pAYb1t0\$20#tRAde"
+db_host="ec2-13-52-131-42.us-west-1.compute.amazonaws.com"
+db_port="1521"
+db_name="paybito"
+
+# SQL*Plus komutu ile Oracle veritabanına bağlanma
+sqlplus ${db_username}/${db_password}@${db_host}:${db_port}/${db_name}
+
+# Bağlantı başarılı mesajı gösterme ve log dosyasına yazdırma
+if [ $? -eq 0 ]
+then
+    echo "Oracle veritabanına başarıyla bağlandınız."
+    echo "Oracle veritabanına başarıyla bağlandınız." >> log.txt
+else
+    echo "Oracle veritabanına bağlanırken bir hata oluştu."
+    echo "Oracle veritabanına bağlanırken bir hata oluştu." >> log.txt
+fi
