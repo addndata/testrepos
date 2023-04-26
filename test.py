d9b25d690944b9224e70e666e80892e8878d12cf
addndata <111285262+addndata@users.noreply.github.com>
2023-04-27 02:30:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import os
+import socket
 
-os.system("sudo apt-get update")
-os.system("sudo apt-get install gcc")
+HOST = 'localhost'  # ağı dinlemek istediğiniz IP adresi
+PORT = 443        # dinlemek istediğiniz port numarası
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with open("veriler.txt", "w") as f:  # verileri kaydedeceğimiz dosya
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            f.write(data.decode("utf-8"))  # gelen verileri dosyaya yazdırma
