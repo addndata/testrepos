682d931fe02b34893cf2cc68adc0f7e70457871e
addndata <111285262+addndata@users.noreply.github.com>
2024-09-24 05:44:11 +0300

    Update and rename bas.sh to bads.sh

--- bads.sh
+++ bads.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+output_file="secrets.txt"
+
+> "$output_file"
+
 secrets=$(kubectl get secrets -o jsonpath='{.items[*].metadata.name}')
 
 for secret in $secrets; do
-    echo "Secret: $secret"
+    echo "Secret: $secret" >> "$output_file"
     
     keys=$(kubectl get secret "$secret" -o jsonpath='{.data}')
 
     for key in $(echo "$keys" | jq -r 'keys[]'); do
         value=$(kubectl get secret "$secret" -o jsonpath="{.data.$key}" | base64 --decode)
-        echo "$key: $value"
+        echo "$key: $value" >> "$output_file"
     done
     
-    echo "-----------------------------"
+    echo "-----------------------------" >> "$output_file"
 done
-c
+
+echo "++ $output_file ++."
