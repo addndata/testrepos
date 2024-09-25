317fc4409cf226fbc88042cb91648780fedf691e
addndata <111285262+addndata@users.noreply.github.com>
2024-09-25 18:07:38 +0300

    Update fs.sh

--- fs.sh
+++ fs.sh
@@ -4,4 +4,4 @@ curl -X GET "https://api.cloudflare.com/client/v4/zones" \
 -H "Host: api.cloudflare.com" \
 -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36" \
 -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" \
--H "Authorization: Bearer yimFNXbGIo2YUE16w_0gysG-oYUezvFLLLEVAwTn"
+-H "Authorization: Bearer 1"
