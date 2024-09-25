b5e92a878d3fbc010e1e3502fdf6238431e93a85
addndata <111285262+addndata@users.noreply.github.com>
2024-09-25 21:40:46 +0300

    Update and rename mongocek.py to mongocek2.py

--- mongocek2.py
+++ mongocek2.py
@@ -1,13 +1,13 @@
 from pymongo import MongoClient
 
 # MongoDB connection string
-connection_string = "mongodb://waves-exchange-landing:iesheBaegeel1ue7xohtahGh@mongo-htz-hel1-1.wvservices.com:27017/waves-exchange-landing"
+connection_string = "mongodb://waves-enterprise-v2-prod:sza5VzRMCl3TMSWqavtA@mongo-htz-hel1-1.wvservices.com:27017/waves-enterprise-v2-prod"
 
 # Connect to MongoDB
 client = MongoClient(connection_string)
 
 # Select the database
-db = client['waves-exchange-landing']
+db = client['waves-enterprise-v2-prod']
 
 # List all collection names
 collection_names = db.list_collection_names()
