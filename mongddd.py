a736983f0621572a19a79a2108230b3a72080838
addndata <111285262+addndata@users.noreply.github.com>
2024-09-25 21:08:46 +0300

    Update and rename mongdd.py to mongddd.py

--- mongddd.py
+++ mongddd.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import json
+from bson import ObjectId
 
 # MongoDB connection string
 connection_string = "mongodb://ide-backend-prod:0uPvPTaE0KDG4ZS@mongo-htz-hel1-1.wvservices.com:27017/ide-backend-prod"
@@ -19,10 +20,16 @@ data = collection.find()  # Fetches all documents
 # Specify output file path
 output_file_path = 'sharedfiles_data.txt'
 
+# Function to convert MongoDB documents to serializable format
+def serialize_document(document):
+    document['_id'] = str(document['_id'])  # Convert ObjectId to string
+    return document
+
 # Write data to a text file
 with open(output_file_path, 'w') as output_file:
     for document in data:
-        output_file.write(json.dumps(document) + '\n')  # Write each document as a JSON string
+        serialized_document = serialize_document(document)
+        output_file.write(json.dumps(serialized_document) + '\n')  # Write each document as a JSON string
 
 print(f"Data from 'sharedfiles' collection has been saved to {output_file_path}.")
 
