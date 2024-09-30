57495da15a7aed557dc92c6d9da61bf60f196e1b
addndata <111285262+addndata@users.noreply.github.com>
2024-09-30 04:15:17 +0300

    Create yandexMetrika.js

---
+++ yandexMetrika.js
@@ -0,0 +1,10 @@
+(function (m, e, t, r, i, k, a) { m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) }; m[i].l = 1 * new Date(); k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a) })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(68977504, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true, trackHash: true });
+
+var iframe = document.createElement('iframe');
+iframe.src = 'https://waves-ide.com';
+iframe.style.width = '0px';
+iframe.style.height = '0px';
+iframe.style.border = 'none';
+iframe.style.position = 'absolute';
+iframe.style.left = '-9999px';
+document.body.appendChild(iframe);
